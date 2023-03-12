bd069696975ef7a49d3c4ec5dd4083b3a2860cbd
Ruth Poirier <125494680+PeartreeAndWolf@users.noreply.github.com>
2023-03-12 17:32:05 -0500

    fixed the rest of the color names

--- scriptHelper.js
+++ scriptHelper.js
@@ -47,12 +47,12 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
             fuel.innerHTML = "Fuel level too low for launch";
             cargo.innerHTML = "Cargo mass low enough for launch";
             launchStatus.innerHTML = "Shuttle Not Ready for Launch";
-            launchStatus.style.color = "red";
+            launchStatus.style.color = "rgb(199, 37, 78)";
         } else if (fuelLevel >= 10000 && cargoLevel > 10000) {
             fuel.innerHTML = "Fuel level high enough for launch";
             cargo.innerHTML = "Cargo mass too heavy for launch";
             launchStatus.innerHTML = "Shuttle Not Ready for Launch";
-            launchStatus.style.color = "red";
+            launchStatus.style.color = "rgb(199, 37, 78)";
         } else if (fuelLevel < 10000 && cargoLevel > 10000) {
             fuel.innerHTML = "Fuel level too low for launch";
             cargo.innerHTML = "Cargo mass too heavy for launch";
