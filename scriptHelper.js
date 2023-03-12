4dfc390d4fa519335c4f69a022fdcd32cd85fb04
Ruth Poirier <125494680+PeartreeAndWolf@users.noreply.github.com>
2023-03-12 16:56:44 -0500

    fixed typo Element

--- scriptHelper.js
+++ scriptHelper.js
@@ -3,7 +3,7 @@ require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
    // Here is the HTML formatting for our mission target div.
-    let div = document.getElmentById("missionTarget");
+    let div = document.getElementById("missionTarget");
     div.innerHTML = 
                 `<h2>Mission Destination</h2>
                 <ol>
