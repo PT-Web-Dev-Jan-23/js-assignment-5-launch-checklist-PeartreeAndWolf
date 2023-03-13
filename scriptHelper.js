2d0f722664cac15f4493dbbc05c3d88b574a7eb9
Ruth Poirier <125494680+PeartreeAndWolf@users.noreply.github.com>
2023-03-13 11:49:37 -0500

    fixed bug in scriptHelper.js ln 68

--- scriptHelper.js
+++ scriptHelper.js
@@ -65,7 +65,7 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
             fuelStatus.innerHTML = "Fuel level high enough for launch";
             cargoStatus.innerHTML = "Cargo mass low enough for launch";
             launchStatus.innerHTML = "Shuttle is Ready for Launch";
-            launchStatus.style.color = "rgb(65, 159, 106";
+            launchStatus.style.color = "rgb(65, 159, 106)";
         
 
         }
